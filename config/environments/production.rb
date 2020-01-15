b43fadc2e7ca80c9242e812eeb1f0d20627bb17d
EC2 Default User <ec2-user@ip-172-31-43-148.ap-northeast-1.compute.internal>
2020-01-15 08:21:23 +0000

    edit default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'https://dackelteckelprofy.herokuapp.com/' }
+
 end
